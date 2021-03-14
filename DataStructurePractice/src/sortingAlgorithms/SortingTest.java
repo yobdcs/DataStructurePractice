b23cb2acc3f428ee51d7a1ad6605f5d01ea6fcc9
yobdcs <77945923+yobdcs@users.noreply.github.com>
2021-03-15 02:07:11 +0800

    Update SortingTest.java

--- DataStructurePractice/src/sortingAlgorithms/SortingTest.java
+++ DataStructurePractice/src/sortingAlgorithms/SortingTest.java
@@ -7,11 +7,13 @@ public class SortingTest {
 	public static void main(String[] args) {
 		SortingAlgorithum sa = new SortingAlgorithum();
 
-		int[] a1 = {3, 2, 4, 1, 11, 8, 7, 2};
-//		int[] a1 = {7, 1};
+		int[] a1 = {3, 2, 7, 1, 11, 8, 7, 2};
+//		int[] a1 = {2, 4, 1, 5, 3};
 //		System.out.println("bubble sort: "+ Arrays.toString(sa.bubbleSort(a1)));
 //		System.out.println("selection sort: "+ Arrays.toString(sa.selectionSort(a1)));
-		
+//		System.out.println("insertion sort: "+ Arrays.toString(sa.insertionSort(a1)));
+		System.out.println("merge sort: "+ Arrays.toString(sa.mergeSort(a1)));
+//		System.out.println("merge sort: "+ Arrays.toString(sa.quickSort(a1)));
 	}
 
 }
