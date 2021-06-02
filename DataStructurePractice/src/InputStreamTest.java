c43e8e110d5f1c5036eefa6ebef76dfc63d5221b
Mag <Mag@DESKTOP-9VL2PL9>
2021-06-02 14:48:35 +0800

    6/2 temp

---
+++ DataStructurePractice/src/InputStreamTest.java
@@ -0,0 +1,14 @@
+import java.io.FileInputStream;
+
+public class InputStreamTest {
+
+	private FileInputStream f1;
+	private FileInputStream f2;
+	
+	public InputStreamTest() {
+	}
+	
+	public void testMerge() {
+		
+	}
+}
--- DataStructurePractice/src/Test.java
+++ DataStructurePractice/src/Test.java
@@ -1,26 +1,37 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Test {
 
 	public static void main(String[] args) {
-
-		long data = 21474836;
+		InputStreamTest ist = new InputStreamTest();
 		
-		var a = longtoBytes(data);
-
-		System.out.println(4);
+		try {
+			InputStream is1 = new FileInputStream("E:\\test01.jpg");
+			InputStream is2 = new FileInputStream("E:\\test02.jpg");
+			
+			System.out.println(is1.readAllBytes().length);
+			System.out.println(is2.readAllBytes().length);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
-	private static byte[] longtoBytes(long data) {
-		 return new byte[]{
-		 (byte) ((data >> 56) & 0xff),
-		 (byte) ((data >> 48) & 0xff),
-		 (byte) ((data >> 40) & 0xff),
-		 (byte) ((data >> 32) & 0xff),
-		 (byte) ((data >> 24) & 0xff),
-		 (byte) ((data >> 16) & 0xff),
-		 (byte) ((data >> 8) & 0xff),
-		 (byte) ((data >> 0) & 0xff),
-		 };
-		}
+//	private static byte[] longtoBytes(long data) {
+//		 return new byte[]{
+//		 (byte) ((data >> 56) & 0xff),
+//		 (byte) ((data >> 48) & 0xff),
+//		 (byte) ((data >> 40) & 0xff),
+//		 (byte) ((data >> 32) & 0xff),
+//		 (byte) ((data >> 24) & 0xff),
+//		 (byte) ((data >> 16) & 0xff),
+//		 (byte) ((data >> 8) & 0xff),
+//		 (byte) ((data >> 0) & 0xff),
+//		 };
+//	}
 	
 }
