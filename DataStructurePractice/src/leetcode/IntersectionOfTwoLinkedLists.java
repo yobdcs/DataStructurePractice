c644f5d93f1f84868c7b4e33443289a892339283
Mag <Mag@DESKTOP-9VL2PL9>
2021-06-07 21:59:30 +0800

    6/7 leetcode

--- DataStructurePractice/src/leetcode/IntersectionOfTwoLinkedLists.java
+++ DataStructurePractice/src/leetcode/IntersectionOfTwoLinkedLists.java
@@ -1,10 +1,90 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class IntersectionOfTwoLinkedLists {
 
+	/*	Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+	 * 
+	 * 	If the two linked lists have no intersection at all, return null.
+	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
+	public class ListNode {
+		
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+	
+	private static ListNode intersectionOfTwoLinkedLists(ListNode headA, ListNode headB) {
+		Set<ListNode> set = new HashSet<ListNode>();
+		
+		while(headA != null) {
+			set.add(headA);
+			headA = headA.next;
+		}
+		
+		while(headB != null) {
+			if(set.contains(headB))
+				return headB;
+				
+			headB = headB.next;
+		}
+		return null;
+	}
+	
+	private static ListNode intersectionOfTwoLinkedLists2(ListNode headA, ListNode headB) {
+		int lengthA = 0, lengthB = 0;
+		ListNode pointA = headA, pointB = headB;
+		
+		while(pointA != null || pointB != null) {
+			if(pointA != null) {
+				pointA = pointA.next;
+				lengthA ++;
+			}
+			
+			if(pointB != null) {
+				pointB = pointB.next;
+				lengthB ++;
+			}				
+		}
+		
+		while(headA != headB) {
+			if(lengthA > lengthB) {
+				headA = headA.next;
+				lengthA --;
+				continue;
+			}
+
+			if(lengthB > lengthA) {
+				headB = headB.next;
+				lengthB --;
+				continue;
+			}
+
+			headA = headA.next;
+			headB = headB.next;
+		}
+		return headA;
+	}
+	
+	private static ListNode intersectionOfTwoLinkedLists3(ListNode headA, ListNode headB) {
+		ListNode pointA = headA, pointB = headB;
+		
+		while(pointA != pointB) {
+			pointA = pointA == null ? headB : pointA.next;
+			pointB = pointB == null ? headA : pointB.next;
+		}
+		return pointA;
+	}
+	
 }
---
+++ DataStructurePractice/src/leetcode/TwoSumII_InputArrayIsSorted.java
@@ -0,0 +1,73 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumII_InputArrayIsSorted {
+
+	/*	Given an array of integers numbers that is already sorted in non-decreasing order, 
+	 * 	
+	 * 	find two numbers such that they add up to a specific target number.
+	 * 
+	 * 	Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, 
+	 * 
+	 * 	where 1 <= answer[0] < answer[1] <= numbers.length.
+	 * 
+	 * 	The tests are generated such that there is exactly one solution. You may not use the same element twice.
+	 */
+	
+	public static void main(String[] args) {
+		int[] numbers = {2, 7, 11, 15};
+		int target = 22;
+
+		System.out.println(Arrays.toString(twoSumII_InputArrayIsSorted4(numbers, target)));
+	}
+
+	private static int[] twoSumII_InputArrayIsSorted(int[] numbers, int target) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		for(int i = 0 ; i < numbers.length ; i ++) {
+			if(map.containsKey(target - numbers[i]))
+				return new int[] {map.get(target - numbers[i]) + 1, i + 1};
+			
+			map.put(numbers[i], i);
+		}
+		return null;
+	}
+	
+	private static int[] twoSumII_InputArrayIsSorted2(int[] numbers, int target) {
+		int temp = 0;
+		for(int i = 0 ; i < numbers.length ; i ++) {
+			temp = target - numbers[i];
+			for(int j = numbers.length - 1 ; j > i ; j --) {
+				if(temp == numbers[j])
+					return new int[] {i + 1, j + 1};
+
+				if(temp > numbers[j])
+					break;
+			}
+		}
+		return null;
+	}
+	
+	private static int[] twoSumII_InputArrayIsSorted3(int[] numbers, int target) {
+		int i = 0, j = numbers.length - 1;
+		while(i < j) {
+			if(numbers[i] + numbers[j] < target)
+				i ++;
+			
+			if(numbers[i] + numbers[j] > target)
+				j --;
+			
+			if(numbers[i] + numbers[j] == target)
+				return new int[] {i + 1, j + 1};
+		}
+		return null;
+	}
+	
+	private static int[] twoSumII_InputArrayIsSorted4(int[] numbers, int target) {
+		//binary search
+	}
+	
+}
